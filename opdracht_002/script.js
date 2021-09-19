852e3d218485d660714f66467aefd5980a854d26
MarloesVeger <89524035+MarloesVeger@users.noreply.github.com>
2021-09-19 14:29:31 +0200

    Add files via upload

---
+++ opdracht_002/script.js
@@ -0,0 +1,6 @@
+console.log("Hallo Winc Academy studenten")
+// Dit is een grote som
+/* let sum = 1230941 + 1823794;
+console.log("sum:", sum);
+let division = 637263 / 54;
+console.log("division:", division); */
